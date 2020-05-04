a9f7ecc9caf8595a03ab23712211099d99df61b4
naveenrs123 <naveen.sivasankar1234@gmail.com>
2020-05-04 17:42:04 -0500

    Basically finished

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
-    var coll = document.getElementsByClassName("collapsible");
-    var i;
+    $("select").change(function(){
+        $(this).find("option:selected").each(function(){
+            var optionValue = $(this).attr("value");
+            if(optionValue){
+                var active = "." + optionValue;
+                $(".collapsible-content").not(active).hide();
+                $(active).animate({
+                  height: "toggle"
+                }, 800, function() {
+                });
 
-    for (i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            var content = this.nextElementSibling;
-            if (content.style.maxHeight){
-            content.style.maxHeight = null;
-            content.style.padding = "0 20px";
-            } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-            content.style.padding = "20px";
+            } else{
+                $(".collapsible-content").hide();
             }
         });
-    }
+    }).change();
 
     // Add smooth scrolling to all links
     $("a").on('click', function(event) {
@@ -26,9 +26,16 @@ $(document).ready(function(){
         var hash = this.hash;
         // Using jQuery's animate() method to add smooth page scroll
         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top - 80
-        }, 800);
+        if ($(window).width() > 900) {
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top - 80
+          }, 800);
+        } else {
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top - 120
+          }, 1000);
+        }
+
         location.hash = hash;
       } // End if
     });
