ae78e6dadbde4976b7693c0802aebc00d9b20883
naveenrs123 <naveen.sivasankar1234@gmail.com>
2020-05-03 21:32:48 -0500

    Added smoother animations and completed more of the basic framework

---
+++ script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.maxHeight){
+            content.style.maxHeight = null;
+            content.style.padding = "0 20px";
+            } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+            content.style.padding = "20px";
+            }
+        });
+    }
+
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+        // Store hash
+        var hash = this.hash;
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top - 80
+        }, 800);
+        location.hash = hash;
+      } // End if
+    });
+  });
+
+
+  
