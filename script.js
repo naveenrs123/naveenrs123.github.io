d59ba8948da2662afa91c611acc2c96f3e022d82
naveenrs123 <naveen.sivasankar1234@gmail.com>
2021-05-17 22:17:41 -0700

    Add sidebar image, change font, improve styling

--- script.js
+++ script.js
@@ -17,6 +17,19 @@ $(document).ready(function () {
         closeNav();
         document.querySelector("main").style.marginRight = "0";
     }
+
+    $("a").on('click', function(event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+          event.preventDefault();
+          var hash = this.hash;
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 500);
+  
+        } // End if
+      });
+
 });
 
 // Updates icon when toggling collapsed state of about section links.
